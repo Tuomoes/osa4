1b98b2fa23b2288ef7b4b1e7075daeb0ac4086a3
Tuomo Eskola <tuomo_eskola@hotmail.com>
2018-07-20 16:01:59 +0300

    subtask 4.5* done.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,8 +7,17 @@ const totalLikes = (blogs) => {
         return likeSum + blog.likes
     }, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    // return blogs.sort((a, b) => b.likes - a.likes)[0]
+    return blogs[blogs.reduce((indexOfFavorite, blog, index, blogs) => {
+        if (blog.likes > blogs[indexOfFavorite].likes) return index
+        else return indexOfFavorite
+    }, 0)]
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
