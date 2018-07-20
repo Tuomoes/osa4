fa49883ff074e3fb841be05e8cd8fffb1691a50f
Tuomo Eskola <tuomo_eskola@hotmail.com>
2018-07-20 12:44:30 +0300

    subtask 4.2 models & controllers don, but unit tests not

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-module.exports = Blog
-
+app.use('/api/blogs', blogsRouter)
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -25,23 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
