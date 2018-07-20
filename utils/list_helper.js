d27e96afc27f830970c587d88cd5e2442320cccb
Tuomo Eskola <tuomo_eskola@hotmail.com>
2018-07-20 14:51:29 +0300

    subtask 4.4 done.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((likeSum, blog) => {
+        return likeSum + blog.likes
+    }, 0)
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
