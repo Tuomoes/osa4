dca382f419dd2fcf30958e03b00c21869dad6d3a
Tuomo Eskola <tuomo_eskola@hotmail.com>
2018-07-24 10:21:18 +0300

    subtask 4.9 done

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -50,7 +50,31 @@ describe('when some blogs are initially saved', async () => {
         expect(response.body.length).toBe(initialBlogs.length)
         console.log('blog get test finished')
     })
-    
+
+    test('POST /api/blogs succeessful with valid data', async () => {
+        console.log('starting blog POST test')
+        const newBlog = {
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const responseGet = await api.get('/api/blogs')
+        const notesAtStartLength = initialBlogs.length
+        const titles = responseGet.body.map(r => r.title)
+
+        expect(responseGet.body.length).toBe(notesAtStartLength + 1)
+        expect(titles).toContain('Canonical string reduction')
+        console.log('finished blog POST test')
+    })
+
     afterAll(() => {
         server.close()
     })
